287f3967ccc290922c689a02da40708ce77fe1ca
endrollex <seebook@126.com>
2014-07-21 10:10:32 +0800

    ngon problem

--- export_static.py
+++ export_static.py
@@ -17,13 +17,13 @@ fw = open(export, 'w')
 write_str = ""
 count = 0
 for v in mesh.vertices:
-    temp = str(v.co)+str(v.normal)
-    temp = temp.replace(")><Vector (", " ")
-    temp = temp.replace("<Vector (", "")
-    temp = temp.replace(",", "")
-    temp = temp.replace(")>", "")
-    write_str += temp+"\n"
-    count += 1
+	temp = str(v.co)+str(v.normal)
+	temp = temp.replace(")><Vector (", " ")
+	temp = temp.replace("<Vector (", "")
+	temp = temp.replace(",", "")
+	temp = temp.replace(")>", "")
+	write_str += temp+"\n"
+	count += 1
 write_str = str(count)+"\n"+write_str
 fw.write(write_str)
 fw.close()
@@ -34,14 +34,19 @@ export = export_dir+export_f_triangle
 fw = open(export, 'w')
 write_str = ""
 count = 0
+is_ngon = False
 for t in mesh.tessfaces:
-    if len(t.vertices) == 3:
-        write_str += str(t.vertices[0])+" "+str(t.vertices[1])+" "+str(t.vertices[2])+"\n"
-        count += 1
-    else:
-        write_str += str(t.vertices[0])+" "+str(t.vertices[1])+" "+str(t.vertices[2])+"\n"
-        write_str += str(t.vertices[0])+" "+str(t.vertices[2])+" "+str(t.vertices[3])+"\n"
-        count += 2
+	if len(t.vertices) == 3:
+		write_str += str(t.vertices[0])+" "+str(t.vertices[1])+" "+str(t.vertices[2])+"\n"
+		count += 1
+	else:
+		if len(t.vertices) > 4:
+			is_ngon = True
+		write_str += str(t.vertices[0])+" "+str(t.vertices[1])+" "+str(t.vertices[2])+"\n"
+		write_str += str(t.vertices[0])+" "+str(t.vertices[2])+" "+str(t.vertices[3])+"\n"
+		count += 2
 write_str = str(count)+"\n"+write_str
+if is_ngon:
+	write_str = "ngon detected, can not export triangle"
 fw.write(write_str)
 fw.close()
--- show_static_structure.py
+++ show_static_structure.py
@@ -17,24 +17,24 @@ print("Normal:\t\t\t", mesh.vertices[0].normal)
 
 # texture
 try:
-    print("Tex-Coords:\t\t", mesh.uv_layers[0].data[0].uv)
-    mesh.calc_tangents()
-    print("Tangent:\t\t", mesh.loops[0].tangent)
-    mesh.free_tangents()
-    print("DiffuseMap:\t\t", mesh.uv_textures[0].data[0].image.filepath)
-    print("NormalMap:\t\t")
+	print("Tex-Coords:\t\t", mesh.uv_layers[0].data[0].uv)
+	mesh.calc_tangents()
+	print("Tangent:\t\t", mesh.loops[0].tangent)
+	mesh.free_tangents()
+	print("DiffuseMap:\t\t", mesh.uv_textures[0].data[0].image.filepath)
+	print("NormalMap:\t\t")
 except:
-    print("No Texture Data")
+	print("No Texture Data")
 
 # material
 try:
-    print("Ambient:\t\t", mesh.materials[0].ambient)
-    print("Diffuse:\t\t", mesh.materials[0].diffuse_color)
-    print("Diffuse Intensity:\t", mesh.materials[0].diffuse_intensity)
-    print("Specular:\t\t", mesh.materials[0].specular_color)
-    print("Specular Intensity:\t", mesh.materials[0].specular_intensity)
-    print("Specular Power:\t\t", mesh.materials[0].specular_hardness) # ShininessExponent
-    print("Reflect:\t\t", mesh.materials[0].mirror_color) # ReflectionColor
-    print("Reflect Intensity:\t", mesh.materials[0].raytrace_mirror.reflect_factor)
+	print("Ambient:\t\t", mesh.materials[0].ambient)
+	print("Diffuse:\t\t", mesh.materials[0].diffuse_color)
+	print("Diffuse Intensity:\t", mesh.materials[0].diffuse_intensity)
+	print("Specular:\t\t", mesh.materials[0].specular_color)
+	print("Specular Intensity:\t", mesh.materials[0].specular_intensity)
+	print("Specular Power:\t\t", mesh.materials[0].specular_hardness) # ShininessExponent
+	print("Reflect:\t\t", mesh.materials[0].mirror_color) # ReflectionColor
+	print("Reflect Intensity:\t", mesh.materials[0].raytrace_mirror.reflect_factor)
 except:
-    print("No Material Data")
+	print("No Material Data")
