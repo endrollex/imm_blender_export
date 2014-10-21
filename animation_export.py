5c2701b00ac166c83e1654a00df323d797e4f449
endrollex <seebook@126.com>
2014-10-21 16:41:04 +0800

    still working

--- animation_export.py
+++ animation_export.py
@@ -15,6 +15,7 @@ o_mesh = bpy.data.objects[2]
 
 # ini var
 mat_to_root = []
+mat_offset = []
 
 # get index
 def get_index(item, bpy_data):
@@ -33,8 +34,9 @@ def to_parent(ix):
 	# if have not parent, return to_root
 	# assume there is only one root bone
 	if arma.bones[ix].parent == None:
-		return o_arma.matrix_basis
-	return mathutils.Matrix.Translation(arma.bones[ix].parent.tail_local - arma.bones[ix].parent.head_local)
+		return mathutils.Matrix.Identity(4)
+	return mathutils.Matrix.transposed(
+		mathutils.Matrix.Translation(arma.bones[ix].parent.tail_local - arma.bones[ix].parent.head_local))
 
 # to root i
 def to_root_i(ix):
@@ -44,6 +46,7 @@ def to_root_i(ix):
 # calc to root i
 for ixl in range(0, len(arma.bones)):
 	mat_to_root.append(None)
+	mat_offset.append(None)
 mat_to_root[0] = to_parent(0)
 for ixl in range(0, len(arma.bones)):
 	to_root_i(ixl)
@@ -51,5 +54,15 @@ for ixl in range(0, len(mat_to_root)):
 	print("bone", ixl)
 	print(mat_to_root[ixl])
 
-# offset, mesh to armature
-offset = mathutils.Matrix.Identity(4)
+# offset transformation, mesh to armature
+def offset_trans(ix):
+	None
+
+# test
+vec = mathutils.Vector((1.0, 2.0, 3.0))
+mat_trans = mathutils.Matrix.transposed(
+	mathutils.Matrix.Translation(arma.bones[3].parent.tail_local - arma.bones[3].parent.head_local))
+print("test")
+print(mat_trans)
+print(vec*mat_trans)
+print(o_arma.matrix_basis)
